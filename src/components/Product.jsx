52bc121d699350c82de16fb73a49d83bb4455a2b
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-03 18:21:42 +0530

    Integrated API to display product details

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,84 @@
-import Carousel from "./Carousel";
-import { IMAGE_URLS } from "./constants";
+import { useEffect, useState } from "react";
 
-const Product = () => (
-  <div className="px-6 pb-6">
-    <div>
-      <p className="py-2 text-4xl font-semibold">Infinix INBOOK</p>
-      <hr className="border-2 border-black" />
-    </div>
-    <div className="mt-6 flex gap-4">
-      <div className="w-2/5">
-        <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
+import axios from "axios";
+import { Spinner, Typography } from "neetoui";
+import { isNotNil, append } from "ramda";
+
+import Carousel from "./Carousel";
+
+const Product = () => {
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const {
+    name,
+    description,
+    mrp,
+    offer_price: offerPrice,
+    image_urls: imageUrls,
+    image_url: imageUrl,
+  } = product;
+  const totalDiscount = mrp - offerPrice;
+  const discountPercentage = ((totalDiscount / mrp) * 100).toFixed(2);
+
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+      );
+      setProduct(response.data);
+    } catch (error) {
+      console.log("An error occurred while fetching the data:", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner />
       </div>
-      <div className="w-3/5 space-y-4">
-        <p>
-          Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year Warranty.
-        </p>
-        <p>MRP: $395.97</p>
-        <p className="font-semibold">Offer price: $374.43</p>
-        <p className="font-semibold text-green-600">6% off</p>
+    );
+  }
+
+  return (
+    <div className="px-6 pb-6">
+      <div>
+        <Typography className="py-2 text-4xl font-semibold">{name}</Typography>
+        <hr className="border-2 border-black" />
+      </div>
+      <div className="mt-6 flex gap-4">
+        <div className="mt-16 flex gap-4">
+          <div className="w-2/5">
+            <div className="flex justify-center gap-16">
+              {isNotNil(imageUrls) ? (
+                <Carousel
+                  imageUrls={append(imageUrl, imageUrls)}
+                  title={name}
+                />
+              ) : (
+                <img alt={name} className="w-48" src={imageUrl} />
+              )}
+            </div>
+          </div>
+          <div className="w-3/5 space-y-4">
+            <Typography>{description}</Typography>
+            <Typography>MRP: ${mrp}</Typography>
+            <Typography className="font-semibold">
+              Offer price: ${offerPrice}
+            </Typography>
+            <Typography className="font-semibold text-green-600">
+              {discountPercentage}% off
+            </Typography>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Product;
