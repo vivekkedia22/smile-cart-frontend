7b8dd2bcc445214b92db84a1024e56d79e12f25c
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-04 00:22:52 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import Home from "components/Home";
+import PageNotFound from "components/PageNotFound";
 import Product from "components/Product";
+import { NavLink, Route, Switch } from "react-router-dom";
 
-import "./App.css";
-
-const App = () => <Product />;
+const App = () => (
+  <>
+    <div className="flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route exact component={Product} path="/product" />
+      <Route exact component={Home} path="/" />
+      <Route exact component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
