03353e9b55f5abd31bdb51d41eeb0bb8a3949d6a
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-03 18:54:33 +0530

    Created product API connector file

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import axios from "axios";
+import productsApi from "apis/products";
 import { Spinner, Typography } from "neetoui";
 import { isNotNil, append } from "ramda";
 
@@ -23,9 +23,7 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await axios.get(
-        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
-      );
+      const response = await productsApi.show();
       setProduct(response.data);
     } catch (error) {
       console.log("An error occurred while fetching the data:", error.message);
