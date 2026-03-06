cccab573d9242d97180fd2fdfee8a5782556b092
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-06 17:48:38 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
-
-import productsApi from "apis/products";
 import { Header, PageLoader } from "components/common/index";
 import { MRP, OFFER_PRICE } from "components/constants";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
-import { NoData, Toastr } from "neetoui";
+import { NoData } from "neetoui";
 import { isEmpty, keys } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
 import withTitle from "utils/withTitle";
@@ -14,43 +12,11 @@ import PriceCard from "./PriceCard";
 import ProductCard from "./ProductCard";
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
 
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
-
-  const slugs = keys(cartItems);
-  const totalMrp = cartTotalOf(products, MRP);
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
-
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productsApi.show(slug))
-      );
-      setProducts(responses);
-      const slugsToRemove = [];
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          slugsToRemove.push(slug);
-          Toastr.error(
-            `${name} is no longer available and has been removed from the cart`,
-            { autoClose: 2000 }
-          );
-        }
-      });
-    } catch (error) {
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
+  const totalMrp = cartTotalOf(products, MRP);
 
   if (isLoading) {
     return <PageLoader />;
--- src/hooks/reactQuery/useProductsApi.js
+++ src/hooks/reactQuery/useProductsApi.js
@@ -1,7 +1,12 @@
 import { QUERY_KEYS } from "constants/query";
 
 import productsApi from "apis/products";
-import { useQuery } from "react-query";
+import { existsBy } from "neetocist";
+import { Toastr } from "neetoui";
+import { prop } from "ramda";
+import { useTranslation } from "react-i18next";
+import { useQueries, useQuery } from "react-query";
+import useCartItemsStore from "stores/useCartItemsStore";
 
 export const useShowProduct = slug =>
   useQuery({
@@ -16,3 +21,29 @@ export const useFetchProducts = params =>
     keepPreviousData: true,
     // enabled: !!params.searchTerm,
   });
+
+export const useFetchCartProducts = slugs => {
+  const { t } = useTranslation();
+  const { cartItems, setSelectedQuantity } = useCartItemsStore();
+
+  const responses = useQueries(
+    slugs.map(slug => ({
+      queryKey: [QUERY_KEYS.PRODUCTS, slug],
+      queryFn: () => productsApi.show(slug),
+      onSuccess: ({ availableQuantity, name }) => {
+        if (availableQuantity >= cartItems[slug]) return;
+
+        setSelectedQuantity(slug, availableQuantity);
+        if (availableQuantity === 0) {
+          Toastr.error(t("product.error.removedFromCart", { name }), {
+            autoClose: 2000,
+          });
+        }
+      },
+    }))
+  );
+  const data = responses.map(prop("data")).filter(Boolean);
+  const isLoading = existsBy({ isLoading: true }, responses);
+
+  return { data, isLoading };
+};
