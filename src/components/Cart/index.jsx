c77044ead57165252c74a5d7ef837411daf8e6dc
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-06 11:47:04 +0530

    Created HOC to add page title

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -4,9 +4,11 @@ import productsApi from "apis/products";
 import { Header, PageLoader } from "components/common/index";
 import { MRP, OFFER_PRICE } from "components/constants";
 import { cartTotalOf } from "components/utils";
+import i18n from "i18next";
 import { NoData, Toastr } from "neetoui";
 import { isEmpty, keys } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
+import withTitle from "utils/withTitle";
 
 import PriceCard from "./PriceCard";
 import ProductCard from "./ProductCard";
@@ -84,4 +86,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default withTitle(Cart, i18n.t("cart.title"));
