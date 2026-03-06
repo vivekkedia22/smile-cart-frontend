135fd1c11141b4a5f22703f14fceb841f644f715
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-06 13:56:04 +0530

    Initialized query client instance

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import initializeAxios from "apis/axios";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
@@ -16,10 +17,12 @@ initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ToastContainer />
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider>
+      <BrowserRouter>
+        <ToastContainer />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
