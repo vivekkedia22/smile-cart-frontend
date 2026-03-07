51d665d7436306b735fcae8703f3067b846a5d55
vivekkedia22 <vivekkedia22@gmail.com>
2026-03-08 02:40:23 +0530

    Implemented yup validation for checkout form

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -8,7 +8,10 @@ import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import withTitle from "utils/withTitle";
 
-import { CHECKOUT_FORM_INITIAL_VALUES } from "./constants";
+import {
+  CHECKOUT_FORM_INITIAL_VALUES,
+  CHECKOUT_FORM_VALIDATION_SCHEMA,
+} from "./constants";
 import Form from "./Form";
 
 const Checkout = () => {
@@ -29,7 +32,10 @@ const Checkout = () => {
   return (
     <NeetoUIForm
       formProps={{ noValidate: true }}
-      formikProps={{ initialValues: CHECKOUT_FORM_INITIAL_VALUES }}
+      formikProps={{
+        initialValues: CHECKOUT_FORM_INITIAL_VALUES,
+        validationSchema: CHECKOUT_FORM_VALIDATION_SCHEMA,
+      }}
     >
       <div className="flex space-x-4">
         <div className="m-10 w-1/2">
